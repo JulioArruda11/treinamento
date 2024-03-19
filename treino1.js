5cadbc40d9eb62adda670c853f81917f4cd94fed
Julio Arruda <julio.arruda11@gmail.com>
2024-03-19 11:06:49 -0300

    Initial commit

---
+++ treino1.js
@@ -0,0 +1,25 @@
+let n1 = 10;
+let n2 = 20;
+let op = "/";
+let result
+
+switch (op){
+    case "+":
+        result = n1 + n2
+        console.log(result)
+    break;
+    case "-":
+        result = n1 - n2
+        console.log(result)
+    break;
+    case "*":
+        result = n1 * n2
+        console.log(result)
+    break;
+    case "/":
+        result = n1 / n2
+        console.log(result)
+    break;
+    default:
+        console.log("[Erro]")
+}
